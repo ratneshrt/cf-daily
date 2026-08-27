8eb32acf7e78a84cbaf00e2a6cb3d98bd23d7200
Ratnesh Kumar <ratneshrt19@gmail.com>
2026-08-28 03:51:46 +0530

    add: /submit code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -89,6 +89,7 @@ func main() {
 		telegramUserRepository,
 		codeSubmissionRepository,
 		telegramProblemMessageRepository,
+		dailyProblemRepository,
 		telegramClient,
 		githubService,
 		githubStateRepository,
